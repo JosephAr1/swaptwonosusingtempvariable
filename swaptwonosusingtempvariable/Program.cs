fe3f79c4c6e1f6e966e5f50ed7e6eeb523091b7b
JosephAr1 <jonielhet@yahoo.com>
2021-06-13 11:38:59 +1200

    Add project files.

---
+++ swaptwonosusingtempvariable/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace swaptwonosusingtempvariable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, temp;
+            
+            Console.WriteLine("Enter the number for num1:" );
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the number for num2:");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Before Swapping num1 = {0} and num2 = {1}",num1,num2);
+
+            temp = num1;
+            num1 = num2;
+            num2 = temp;
+
+            Console.WriteLine("After Swapping num1 = {0} and num2 = {1}", num1, num2);
+            Console.WriteLine("Press any Key to exit");
+            Console.ReadKey();
+
+        }
+    }
+}
